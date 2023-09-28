846f261a16b780235383972d0196654275936b78
Jibs21 <jayveechavez57@gmail.com>
2023-09-28 22:58:59 +0800

    8

---
+++ client/src/components/message/Message.jsx
@@ -0,0 +1,18 @@
+import "./Message.css";
+import { format } from "timeago.js";
+
+export default function Message({ message, own }) {
+  return (
+    <div className={own ? "message own" : "message"}>
+      <div className="messageTop">
+        <img
+          className="messageImg"
+          src="https://picsum.photos/id/237/200/300"
+          alt=""
+        />
+        <p className="messageText">{message.text}</p>
+      </div>
+      <div className="messageBottom">{format(message.createdAt)}</div>
+    </div>
+  );
+}
---
+++ client/src/pages/register/Register.jsx
@@ -0,0 +1,81 @@
+import { useRef } from "react";
+import "./Register.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+export default function Register() {
+  const username = useRef();
+  const email = useRef();
+  const password = useRef();
+  const ConfirmPassword = useRef();
+  const navigate = useNavigate();
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    if (ConfirmPassword.current.value !== password.current.value) {
+      ConfirmPassword.current.setCustomValidity("Password don't Match!");
+    } else {
+      const user = {
+        username: username.current.value,
+        email: email.current.value,
+        password: password.current.value,
+      };
+      try {
+        await axios.post("/auth/register", user);
+        navigate("/login");
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">PhaseBook</h3>
+          <span className="loginDesc">
+            Connect with Friends and the world around you on PhaseBook.
+          </span>
+        </div>
+        <div className="loginRight">
+          <form className="loginBox" onSubmit={handleClick}>
+            <input
+              placeholder="Username"
+              required
+              ref={username}
+              className="loginInput"
+            />
+            <input
+              placeholder="Email"
+              required
+              ref={email}
+              className="loginInput"
+              type="email"
+            />
+            <input
+              placeholder="Password"
+              required
+              ref={password}
+              className="loginInput"
+              type="password"
+              minLength="6"
+            />
+            <input
+              placeholder="Confirm Password"
+              required
+              ref={ConfirmPassword}
+              className="loginInput"
+              type="password"
+              minLength="6"
+            />
+            <button className="loginButton" type="submit">
+              Sign Up
+            </button>
+            <button className="loginRegisterButton">Log into Account</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
